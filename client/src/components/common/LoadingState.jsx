f4beaf52c801c38010d508b1c4ba02398302657e
lakshay <lakshay8219@gmail.com>
2025-02-07 02:07:19 +0530

    Pre-loader

--- client/src/components/common/LoadingState.jsx
+++ client/src/components/common/LoadingState.jsx
@@ -14,7 +14,10 @@ const BouncingDotsPreloader = ({ size = "large" }) => {
   const colors = ["bg-red-500", "bg-yellow-500", "bg-green-500", "bg-blue-500", "bg-purple-500"]
 
   return (
-    <div className={`w-full h-screen flex items-center justify-center bg-gray-500 ${containerClasses[size]}`}>
+    <div className={`w-full  h-screen flex items-center justify-center ${containerClasses[size]}`}>
+      <div className="hidden md:flex text-[4vw] mb-[2.5vw] font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-500 via-yellow-500 to-purple-500">
+      Loading
+      </div>
       {colors.map((color, index) => (
         <motion.div
           key={index}
